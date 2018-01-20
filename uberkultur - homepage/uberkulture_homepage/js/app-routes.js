21c2b91ba9e213b92774a7fadf0f9c6889a15e8f
seanstraus21 <sstraus21@gmail.com>
2018-01-19 21:01:06 -0600

    add angular framework

---
+++ uberkultur - homepage/uberkulture_homepage/js/app-routes.js
@@ -0,0 +1,9 @@
+'use strict';
+
+window.routes = {
+	"/" : {
+		title : "UK Scriptorium",
+		templateUrl : "partials/landing-page.html",
+		controller : "LandingController"
+	}
+}		
---
+++ uberkultur - homepage/uberkulture_homepage/js/controllers/landing-controller.js
@@ -0,0 +1,7 @@
+angular.module('scriptoriumWebapp').controller('landingController', function (
+	$scope, $rootScope, $window){
+
+	$scope.text = "I am a description";
+	console.log($scope.text);
+
+});
